97602b441c76c7b5081d84cd3937528876bd5fa7
JooyoungChoi <JooyoungChoi14@users.noreply.github.com>
2026-05-14 10:13:13 +0900

    fix: use GeckoSessionSettings.USER_AGENT_MODE_MOBILE constant
    The constant is on GeckoSessionSettings, not GeckoSession.

--- app/src/gecko/java/com/devcompanion/engine/EngineFactory.kt
+++ app/src/gecko/java/com/devcompanion/engine/EngineFactory.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.GeckoSessionSettings
 import org.mozilla.geckoview.GeckoView
 
 /**
@@ -33,7 +34,7 @@ object EngineFactory {
         // Enable tracking protection
         session.settings.useTrackingProtection = true
         // Set mobile user agent
-        session.settings.userAgentMode = GeckoSession.USER_AGENT_MODE_MOBILE
+        session.settings.userAgentMode = GeckoSessionSettings.USER_AGENT_MODE_MOBILE
 
         val rt = getOrCreateRuntime(context)
         session.open(rt)
